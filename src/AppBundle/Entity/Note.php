b81718ad9bed97bbca6866838e7728e28faebb90
Roman Dilmi <roman.dilmi@orange.fr>
2018-03-21 12:27:35 +0100

    DEV : note entity

---
+++ src/AppBundle/Entity/Note.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Note
+ *
+ * @ORM\Table(name="note")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\NoteRepository")
+ */
+class Note
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="note", type="float")
+     */
+    private $note;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_matiere", type="integer")
+     */
+    private $idMatiere;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_user", type="integer")
+     */
+    private $idUser;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set note
+     *
+     * @param float $note
+     *
+     * @return Note
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return float
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+
+    /**
+     * Set idMatiere
+     *
+     * @param integer $idMatiere
+     *
+     * @return Note
+     */
+    public function setIdMatiere($idMatiere)
+    {
+        $this->idMatiere = $idMatiere;
+
+        return $this;
+    }
+
+    /**
+     * Get idMatiere
+     *
+     * @return int
+     */
+    public function getIdMatiere()
+    {
+        return $this->idMatiere;
+    }
+
+    /**
+     * Set idUser
+     *
+     * @param integer $idUser
+     *
+     * @return Note
+     */
+    public function setIdUser($idUser)
+    {
+        $this->idUser = $idUser;
+
+        return $this;
+    }
+
+    /**
+     * Get idUser
+     *
+     * @return int
+     */
+    public function getIdUser()
+    {
+        return $this->idUser;
+    }
+}
+
